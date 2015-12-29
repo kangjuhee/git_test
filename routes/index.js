937562defe2a01ecf621ec648a1183f5136b882a
juheekang <skcjswo8257@naver.com>
2015-12-29 15:42:00 +0900

    index.js 파일에 변수 선언 추가

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,8 @@ mongo.connect('mongodb://127.0.0.01/cha', function(err, db){
   client. on(connect, function(socket){
     var col = db.collection('message');
 
+    var test;
+
     //wait for input
     socket.on('input', function(data){
       console.log(data); //어떻게 돌아가는 지 알 수 있다.
